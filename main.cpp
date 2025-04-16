0a19c3e8f1f9c053c6e92e6eb4b0fda8c1810bff
ヒガシ_サチエ <k024g0072@g.neec.ac.jp>
2025-04-16 15:06:12 +0900

    00_04

--- main.cpp
+++ main.cpp
@@ -1,82 +1,42 @@
 #include <Windows.h>
-#include <cstdint>
+#include <filesystem>
+#include <fstream>
+#include <chrono>
 
-// クライアント領域のサイズ
-const int32_t kClientWindth = 1280;
-const int32_t kClientHeight = 720;
+void Log(std::ostream& os, const std::string& message) {
 
-// ウィンドウプロシージャ
-LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam) {
+	os << message << std::endl;
 
-	// メッセージ処理
-	switch (msg) {
-		// ウィンドウが閉じられた
-	case WM_DESTROY:
-		// アプリの終了
-		PostQuitMessage(0);
-		return 0;
-	}
-
-	return DefWindowProc(hwnd, msg, wparam, lparam);
+	OutputDebugStringA(message.c_str());
 }
 
 // Windowsアプリでのエントリーポイント
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
-	// ウィンドウクラス
-	WNDCLASS wc = {};
+	// ログのディレクトリを表示
+	std::filesystem::create_directory("logs");
 
-	// ウィンドウプロシージャ
-	wc.lpfnWndProc = WindowProc;
-	// ウィンドウクラス名
-	wc.lpszClassName = L"CG2WondowClass";
-	// インスタンスハンドル
-	wc.hInstance = GetModuleHandle(nullptr);
-	// カーソル
-	wc.hCursor = LoadCursor(nullptr, IDC_ARROW);
+	// 現在時間を取得
+	std::chrono::system_clock::time_point now = std::chrono::system_clock::now();
 
-	// ウィンドウクラスの登録
-	RegisterClass(&wc);
+	// ログファイルを秒に
+	std::chrono::time_point<std::chrono::system_clock, std::chrono::seconds>
+		nowSeconds = std::chrono::time_point_cast<std::chrono::seconds>(now);
 
-	// ウィンドウサイズを表す構造体にクライアント領域を入れる
-	RECT wrc = { 0,0,kClientWindth,kClientHeight };
+	// 日本時間に変更
+	std::chrono::zoned_time localTime{ std::chrono::current_zone(),nowSeconds };
 
-	// クライアント領域を元に実際のサイズにwrcを変更してもらう
-	AdjustWindowRect(&wrc, WS_OVERLAPPEDWINDOW, false);
+	// formatを使って年月日_時分秒の文字列に変換
+	std::string dataString = std::format("{:%y%m%d_%H%M%S}", localTime);
 
-	// ウィンドウの生成
-	HWND hwnd = CreateWindow(
-		wc.lpszClassName,          // クラス名
-		L"CG2",                    // タイトルバーのテキスト
-		WS_OVERLAPPEDWINDOW,       // ウィンドウスタイル
-		CW_USEDEFAULT,             // 表示X座標
-		CW_USEDEFAULT,             // 表示Y座標
-		wrc.right - wrc.left,      // ウィンドウの横幅
-		wrc.bottom - wrc.top,      // ウィンドウの縦幅
-		nullptr,                   // 親ウィンドウハンドル
-		nullptr,                   // メニューハンドル
-		wc.hInstance,              // インスタンスハンドル
-		nullptr                    // オプション
-	);
+	// 時刻を使ってファイル名の決定
+	std::string logFilePath = std::string("logs/") + dataString + ".log";
 
-	// ウィンドウの表示
-	ShowWindow(hwnd, SW_SHOW);
+	// ファイルを作って書き込み準備
+	std::ofstream logStream(logFilePath);
 
-	MSG msg = {};
-
-	// ウィンドウのボタンが押されるまでループ
-	while (msg.message != WM_QUIT) {
-
-		// メッセージがあれば処理
-		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)) {
-
-			TranslateMessage(&msg);
-			DispatchMessage(&msg);
-		} else {
-
-			//ゲーム処理
-		}
-	}
+	// 書き込み
+	Log(logStream, "ログの書き込み");
 
 	return 0;
 }
