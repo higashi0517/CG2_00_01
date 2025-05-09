77622c6350b1b2f2d1f1b277858931f0e5d95335
ヒガシ_サチエ <k024g0072@g.neec.ac.jp>
2025-05-09 10:41:04 +0900

    test

---
+++ DepthStencilResource.h
@@ -0,0 +1,38 @@
+#pragma once
+#include <d3d12.h>
+#include "ResourceObject.h"
+#include <cassert>
+
+inline ResourceObject CreateDepthStencilTextureResource(ID3D12Device* device, UINT width, UINT height) {
+	D3D12_RESOURCE_DESC desc{};
+	desc.Dimension = D3D12_RESOURCE_DIMENSION_TEXTURE2D;
+	desc.Width = width;
+	desc.Height = height;
+	desc.DepthOrArraySize = 1;
+	desc.MipLevels = 1;
+	desc.Format = DXGI_FORMAT_D24_UNORM_S8_UINT;
+	desc.SampleDesc.Count = 1;
+	desc.Layout = D3D12_TEXTURE_LAYOUT_UNKNOWN;
+	desc.Flags = D3D12_RESOURCE_FLAG_ALLOW_DEPTH_STENCIL;
+
+	D3D12_HEAP_PROPERTIES heapProps{};
+	heapProps.Type = D3D12_HEAP_TYPE_DEFAULT;
+
+	D3D12_CLEAR_VALUE clearValue{};
+	clearValue.Format = DXGI_FORMAT_D24_UNORM_S8_UINT;
+	clearValue.DepthStencil.Depth = 1.0f;
+	clearValue.DepthStencil.Stencil = 0;
+
+	ID3D12Resource* depthResource = nullptr;
+	HRESULT hr = device->CreateCommittedResource(
+		&heapProps,
+		D3D12_HEAP_FLAG_NONE,
+		&desc,
+		D3D12_RESOURCE_STATE_DEPTH_WRITE,
+		&clearValue,
+		IID_PPV_ARGS(&depthResource)
+	);
+	assert(SUCCEEDED(hr));
+
+	return ResourceObject(depthResource);
+}
