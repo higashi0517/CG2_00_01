1f07039f3b140e8ea75609c1c6c6398d6e366b86
ヒガシ_サチエ <k024g0072@g.neec.ac.jp>
2026-04-03 14:45:37 +0900

    10章

--- project/Game.h
+++ project/Game.h
@@ -1,14 +1,54 @@
 #pragma once
-class Game
-{
-public:
-	// 初期化
-	void Initialize();
-	// 更新
-	void Update();
-	// 終了
-	void Finalize();
-	// 描画
-	void Draw();
-};
 
+#include "WinApp.h"
+#include "GraphicsDevice.h"
+#include "Input.h"
+#include "Sound.h"
+#include "SrvManager.h"
+#include "ImGuiManager.h"
+#include "DebugCamera.h"
+#include "Object3DManager.h"
+#include "Object3D.h"
+#include "SpriteManager.h"
+#include "ParticleManager.h"
+#include "TextureManager.h"
+#include "ModelManager.h"
+#include "ParticleEmitter.h"
+#include "Framework.h"
+
+class Game :public Framework {
+public:
+	Game();
+	~Game();
+
+	void Initialize() override;
+	void Update() override;
+	void Draw() override;
+	void Finalize() override;
+
+private:
+	// ==== 基盤システム群 ====
+	WinApp* winApp_ = nullptr;
+	GraphicsDevice* graphicsDevice_ = nullptr;
+	Input* input_ = nullptr;
+	Sound* sound_ = nullptr;
+
+	// ==== マネージャー群 ====
+	SrvManager* srvManager_ = nullptr;
+	Object3DManager* object3DManager_ = nullptr;
+	SpriteManager* spriteManager_ = nullptr;
+	ParticleManager* particleManager_ = nullptr;
+	ParticleEmitter* emitter_ = nullptr;
+
+	// ==== ゲームオブジェクト群 ====
+	Camera* camera_ = nullptr;
+	Object3D* object3D_ = nullptr;
+	Object3D* object3D_2_ = nullptr;
+	std::vector<Sprite*> sprites_;
+	Sprite* sprite_ = nullptr;
+
+	// 音声データなど
+	Sound::SoundData bgmData_;
+
+	int selected_ = 0;
+};
